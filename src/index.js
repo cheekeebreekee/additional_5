99a528ee8eecc68e55b5efd50b184dfb944cb786
Yahor Simak <simak_yahor@mail.ru>
2018-01-10 21:44:30 +0300

    add implementation of brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var arr = [];
+  var i = 0;
+  var isRight = true;
+  var char = str.charAt(0);
+  var lastElem = null;
+  while (isRight && char != '') {
+    if (arr.length == 0) {
+      for (var j = 0; j < bracketsConfig.length; j++) {
+        if (bracketsConfig[j][0] == char){
+          arr.push(bracketsConfig[j]);
+          break;
+        }
+        else if (bracketsConfig[j][1] == char) {
+          isRight = false;
+          break;
+        }
+      }
+    }
+    else {
+      lastElem = arr[arr.length-1];
+      if (lastElem[1] == char) {
+        arr.pop();
+      }
+      else {
+        for (var j = 0; j < bracketsConfig.length; j++) {
+          if (bracketsConfig[j][0] == char){
+            arr.push(bracketsConfig[j]);
+            break;
+          }
+          else if (bracketsConfig[j][1] == char) {
+            isRight = false;
+            break;
+          }
+        }
+      }
+    }
+    i++;
+    char = str.charAt(i);
+  }
+  if (arr.length != 0) isRight = false;
+  return isRight;
 }
